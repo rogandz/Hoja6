a8e2578c8905ff8b6307b214c901bec671a58b1c
PaliCR96 <can15053@uvg.edu.gt>
2016-08-29 16:56:43 -0600

    Factory

---
+++ Factory.java
@@ -0,0 +1,12 @@
+
+public class Factory {
+	
+	public Set <Desarrollador> Objeto (){
+		Scanner tipo = new Scanner (System.in);
+		int opcion = 0
+		Object stack;
+		stack = new Object ();
+		
+	}
+
+}

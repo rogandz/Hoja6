867e95d9113519deb1fc12a8b9ca22f6bdf9863a
PaliCR96 <can15053@uvg.edu.gt>
2016-08-29 21:42:40 -0600

    Main

--- Main.java
+++ Main.java
@@ -7,23 +7,20 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		private static Scanner elegido;
-		private static Scanner elegidou;
-		private static Scanner elegidou2;
-		private static Scanner elegidou3;
-		private static Scanner elegidou4;
 		
-		
+				
 		int selec,cont=0;
-	
+		
+		//Creacion del objeto metodos
+		Metodos met = new Metodos();
 		
 		
 		System.out.println("MENU-----------------");
 		System.out.println("Ingrese el numero de la opcion que desea realizar");
 		System.out.println("1. Agregar desarrollador");
-		System.out.println("2. Estadisticas de los desarrolladores ");
+		System.out.println("2. Datos de los desarrolladores ");
 		System.out.println("3. Salir del programa");
-		elegido = new Scanner(System.in);
+		Scanner elegido = new Scanner(System.in);
 		System.out.println();
 		System.out.println("Presione: 1, 2, 3");
 		int entrada = elegido.nextInt();
@@ -34,16 +31,24 @@ public class Main {
 					
 				    if (entrada < 1 || entrada > 3) 
 				    {
-				        System.out.println("Presione: 1, 2, 3");
+				        System.out.println("Presione: 1, 2 o 3");
 				        entrada = elegido.nextInt();
 				    }
 
 				    else if(entrada == 1) //Agregar desarrollador 
 				    {
-				    	
-				    	
-				    	
-				    }//else if 
+				    	met.agregar();
+				    }//else if agregar desarrollador
+				    
+				    else if(entrada == 2) //Datos
+				    {
+				    	met.mostrar();
+				    }//else if estadisticas
+				    
+				    else if(entrada == 3) //Salir
+				    {
+				    	System.exit(0);
+				    }//else if Salir
 				}//while 
 	}//main
 

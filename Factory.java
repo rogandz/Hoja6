e71c16c8387db68137dd9e340180f73a9942394d
PaliCR96 <can15053@uvg.edu.gt>
2016-08-29 18:04:18 -0600

    Factory
    Factory terminado

--- Factory.java
+++ Factory.java
@@ -1,12 +1,53 @@
+import java.util.*;
 
 public class Factory {
 	
-	public Set <Desarrollador> Objeto (){
+	public Set<Desarrollador> Objeto (){
 		Scanner tipo = new Scanner (System.in);
-		int opcion = 0
-		Object stack;
-		stack = new Object ();
+		int opcion = 0;
+		Object stackob;
+		stackob = new Object ();
 		
-	}
+		System.out.println("Que tipo desea implementar?");
+		System.out.println("1. HashSet");
+		System.out.println("2. TreeSet");
+		System.out.println("3. LinkedHashSet");
+		int entrada = tipo.nextInt();
+		
+		while (entrada !=0) 
+		{
+			
+		    if (entrada < 1 || entrada > 3) 
+		    {
+		        System.out.println("Presione: 1, 2 o 3");
+		        entrada = tipo.nextInt();
+		    }
 
+		    else if(entrada == 1) //HashSet
+		    {
+		    	System.out.println("---------HASHSET Seleccionado---------");
+		    	HashSet<Desarrollador> obHash = new HashSet<Desarrollador>();
+		    	stackob = obHash;
+		    	entrada=0;   	
+		    }//else if HashSet
+		    
+		    else if(entrada == 2) //TreeSet
+		    {
+		    	System.out.println("---------TREESET Seleccionado---------");
+		    	TreeSet<Desarrollador> obTree = new TreeSet<Desarrollador>();
+		    	stackob = obTree;
+		    	entrada=0; 
+		    }//else if TreeSet
+		    
+		    else if(entrada == 3) //Salir
+		    {
+		    	System.out.println("---------LINKEDHASHSET Seleccionado---------");
+		    	TreeSet<Desarrollador> obLinked = new TreeSet<Desarrollador>();
+		    	stackob = obLinked;
+		    	entrada=0; 
+		    }//else if Salir
+		}//while 
+		return (Set<Desarrollador>)stackob;
+}//main
+		
 }
